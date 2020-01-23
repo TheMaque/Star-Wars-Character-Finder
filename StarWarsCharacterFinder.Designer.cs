67aaa65c81b811eb80233c0946e1fb762cd1e442
Reginald S. Telemaque <rstelemaque@csusf.local>
2020-01-23 10:53:31 -0500

    Add project files.

---
+++ StarWarsCharacterFinder.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace Star_Wars_Character_Finder
+{
+	partial class StarWarsCharacterFinder
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StarWarsCharacterFinder));
+			this.lstCharacters = new System.Windows.Forms.ListBox();
+			this.picStarWarsLogo = new System.Windows.Forms.PictureBox();
+			this.txtCharacterName = new System.Windows.Forms.TextBox();
+			this.btnFind = new System.Windows.Forms.Button();
+			this.picYoda = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.picStarWarsLogo)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.picYoda)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// lstCharacters
+			// 
+			this.lstCharacters.BackColor = System.Drawing.Color.Black;
+			this.lstCharacters.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lstCharacters.ForeColor = System.Drawing.Color.Yellow;
+			this.lstCharacters.FormattingEnabled = true;
+			this.lstCharacters.ItemHeight = 16;
+			this.lstCharacters.Items.AddRange(new object[] {
+            "ANAKIN",
+            "DARTH VADER",
+            "LUKE",
+            "OBI-WAN",
+            "C-3PO",
+            "R2-D2",
+            "CHEWBACCA",
+            "HAN",
+            "LEIA",
+            "PADME",
+            "QUI-GON",
+            "EMPEROR",
+            "YODA",
+            "JAR JAR",
+            "LANDO",
+            "RED LEADER",
+            "WEDGE",
+            "BAIL ORGANA",
+            "CAPTAIN PANAKA",
+            "MACE WINDU",
+            "NUTE GUNRAY",
+            "BIGGS",
+            "GOLD LEADER",
+            "GENERAL GRIEVOUS",
+            "TARKIN",
+            "PIETT",
+            "FODE/BEED",
+            "RIC OLIE",
+            "SHMI",
+            "RUNE",
+            "BOBA FETT",
+            "OWEN",
+            "ADMIRAL ACKBAR",
+            "WATTO",
+            "LAMA SU",
+            "COUNT DOOKU",
+            "GOLD FIVE",
+            "RED TEN",
+            "SIO BIBBLE",
+            "JABBA",
+            "MAD MADDA",
+            "TAUN WE",
+            "RIEEKAN",
+            "CAPTAIN TYPHO",
+            "ZEV",
+            "DARTH MAUL",
+            "BRAVO TWO",
+            "JANGO FETT",
+            "BOSS NASS",
+            "GENERAL CEEL",
+            "TEY HOW",
+            "TC-14",
+            "KITSTER",
+            "RUWEE",
+            "CLONE COMMANDER CODY",
+            "CLONE COMMANDER GREE",
+            "KI-ADI-MUNDI",
+            "ODD BALL",
+            "MON MOTHMA",
+            "BERU",
+            "DACK",
+            "JANSON",
+            "SEBULBA",
+            "JIRA",
+            "VALORUM",
+            "WALD",
+            "BRAVO THREE",
+            "DOFINE",
+            "TARPALS",
+            "PK-4",
+            "GREEDO",
+            "CLIEGG",
+            "SENATOR ASK AAK",
+            "JOBAL",
+            "ORN FREE TAA",
+            "POGGLE",
+            "SOLA",
+            "SUN RIT",
+            "PLO KOON",
+            "FANG ZAR",
+            "TION MEDON",
+            "ON MEDON",
+            "CAPTAIN ANTILLES",
+            "GIDDEAN DANU",
+            "MOTTI",
+            "CAMIE",
+            "DODONNA",
+            "DERLIN",
+            "OZZEL",
+            "NEEDA",
+            "BIB FORTUNA",
+            "JERJERROD",
+            "BOUSHH",
+            "RABE",
+            "REY",
+            "POE",
+            "SNAP",
+            "GENERAL HUX",
+            "SNOKE",
+            "COLONEL DATOO",
+            "KYLO REN",
+            "JESS",
+            "ELLO ASTY",
+            "NIV LEK",
+            "YOLO ZIFF",
+            "FINN",
+            "ADMIRAL STATURA",
+            "CAPTAIN PHASMA",
+            "MAZ",
+            "BALA-TIK",
+            "LIEUTENANT MITAKA",
+            "UNKAR PLUTT",
+            "LOR SAN TEKKA",
+            "BB-8"});
+			this.lstCharacters.Location = new System.Drawing.Point(43, 104);
+			this.lstCharacters.Name = "lstCharacters";
+			this.lstCharacters.Size = new System.Drawing.Size(378, 180);
+			this.lstCharacters.TabIndex = 0;
+			// 
+			// picStarWarsLogo
+			// 
+			this.picStarWarsLogo.BackColor = System.Drawing.Color.Transparent;
+			this.picStarWarsLogo.BackgroundImage = global::Star_Wars_Character_Finder.Properties.Resources.starwars_logo;
+			this.picStarWarsLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.picStarWarsLogo.Location = new System.Drawing.Point(136, 12);
+			this.picStarWarsLogo.Name = "picStarWarsLogo";
+			this.picStarWarsLogo.Size = new System.Drawing.Size(207, 86);
+			this.picStarWarsLogo.TabIndex = 1;
+			this.picStarWarsLogo.TabStop = false;
+			// 
+			// txtCharacterName
+			// 
+			this.txtCharacterName.BackColor = System.Drawing.Color.Black;
+			this.txtCharacterName.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtCharacterName.ForeColor = System.Drawing.Color.Yellow;
+			this.txtCharacterName.Location = new System.Drawing.Point(43, 309);
+			this.txtCharacterName.Name = "txtCharacterName";
+			this.txtCharacterName.Size = new System.Drawing.Size(300, 20);
+			this.txtCharacterName.TabIndex = 2;
+			// 
+			// btnFind
+			// 
+			this.btnFind.BackColor = System.Drawing.Color.Black;
+			this.btnFind.ForeColor = System.Drawing.Color.Yellow;
+			this.btnFind.Location = new System.Drawing.Point(349, 307);
+			this.btnFind.Name = "btnFind";
+			this.btnFind.Size = new System.Drawing.Size(72, 22);
+			this.btnFind.TabIndex = 3;
+			this.btnFind.Text = "Find";
+			this.btnFind.UseVisualStyleBackColor = false;
+			this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+			// 
+			// picYoda
+			// 
+			this.picYoda.BackColor = System.Drawing.Color.Transparent;
+			this.picYoda.BackgroundImage = global::Star_Wars_Character_Finder.Properties.Resources.baby_yoda;
+			this.picYoda.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.picYoda.Image = global::Star_Wars_Character_Finder.Properties.Resources.baby_yoda;
+			this.picYoda.Location = new System.Drawing.Point(84, 290);
+			this.picYoda.Name = "picYoda";
+			this.picYoda.Size = new System.Drawing.Size(300, 181);
+			this.picYoda.TabIndex = 4;
+			this.picYoda.TabStop = false;
+			// 
+			// StarWarsCharacterFinder
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackgroundImage = global::Star_Wars_Character_Finder.Properties.Resources.starwarscharacterbackground1;
+			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.ClientSize = new System.Drawing.Size(467, 415);
+			this.Controls.Add(this.btnFind);
+			this.Controls.Add(this.txtCharacterName);
+			this.Controls.Add(this.picStarWarsLogo);
+			this.Controls.Add(this.lstCharacters);
+			this.Controls.Add(this.picYoda);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.Name = "StarWarsCharacterFinder";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Star Wars Character Finder";
+			this.Load += new System.EventHandler(this.StarWarsCharacterFinder_Load);
+			((System.ComponentModel.ISupportInitialize)(this.picStarWarsLogo)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.picYoda)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ListBox lstCharacters;
+		private System.Windows.Forms.PictureBox picStarWarsLogo;
+		private System.Windows.Forms.TextBox txtCharacterName;
+		private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.PictureBox picYoda;
+    }
+}
+
---
+++ StarWarsCharacterFinder.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Text;
+using System.Media;
+
+
+namespace Star_Wars_Character_Finder
+{
+	public partial class StarWarsCharacterFinder : Form
+	{
+		PrivateFontCollection starwarsfont = new PrivateFontCollection();
+
+		public StarWarsCharacterFinder()
+		{
+			InitializeComponent();
+		}
+
+		private void StarWarsCharacterFinder_Load(object sender, EventArgs e)
+		{
+			LoadFonts();
+			string[] starwarscharacters = new string[114];
+
+			starwarscharacters[0] = "Anakin";
+			starwarscharacters[1] = "Darth Vader";
+
+		}
+
+		private void AddFont(string font)
+		{
+			// Add Fonts
+			starwarsfont.AddFontFile(font);
+		}
+
+		private void LoadFonts()
+		{
+			//AddFont(Path.Combine(Directory.GetCurrentDirectory(), "Starjedi.ttf"));
+
+			//lstCharacters.Font = new Font(starwarsfont.Families[1], 8, FontStyle.Regular);
+			//txtCharacterName.Font = new Font(starwarsfont.Families[1], 8, FontStyle.Regular);
+			//btnFind.Font = new Font(starwarsfont.Families[1], 8, FontStyle.Regular);
+		}
+
+		private void btnFind_Click(object sender, EventArgs e)
+		{
+
+		}
+	}
+
+
+
+
+}
